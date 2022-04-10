9ecbe4f0d6b16e097f47a05f3e26cbcbd39d0166
Aleksey Ryabykin <ras.unlucky@yandex.ru>
2022-04-10 22:02:33 +0300

    2nd day

--- unks/baseline/python/tracker.py
+++ unks/baseline/python/tracker.py
@@ -1,45 +1,73 @@
 from client2server import client2server
 
 class tracker:
-  def run(tracklog):
-      c2s = client2server()
-      while True:
-          status = c2s.getStatus()
-          dx = int(status) & 0x0fff #0-12 bits
-          state = (int(status) & 0xf000) >> 12 #12-15 bits
-          '''
-          Состояние радара:
-          0 - неопределённое
-          1 - отключен
-          2 - включен
-          3 - перемещается
-          '''
-          pos = (int(status) & 0xf0000) >> 16 #16-19 bits
-          '''
-          Положение радара:
-          0 - неопределённое
-          1 - поворачивается влево
-          2 - поворачивается вправо
-          3 - в крайнем левом положении
-          4 - в крайнем правом положении
-          '''
-          time_stamp = (int(status) & 0x1ffff00000) >> 20 #20-36 bits
-          '''
-          Время в мс с начала работы
-          '''
-          if dx > 2048:
-              dx = dx - 4096
+    def run(tracklog):
+        gamma = 0.85
+        max_speed = 1500
 
-          tracklog.write(f"{dx}".encode())
-          tracklog.write(f"{state}".encode())
-          tracklog.write(f"{pos}".encode())
-          tracklog.write(f"{time_stamp}".encode())
-          
-          if abs(dx) < 1000:
-              if abs(dx) < 5:
-                  c2s.moveStop()
-              else:
-                  if dx > 0:
-                      c2s.moveLeft(5)
-                  else:
-                      c2s.moveRight(5)
+
+        step_counter = 0
+        c2s = client2server()
+        status = c2s.getStatus()
+        dx = int(status) & 0x0fff
+        if dx > 2048:
+                dx = dx - 4096
+
+        previous_dx = dx
+        previous_velocity = 0
+        
+    
+        while True:        
+            dx = int(status) & 0x0fff #0-12 bits
+            state = (int(status) & 0xf000) >> 12 #12-15 bits
+            '''
+            Состояние радара:
+            0 - неопределённое
+            1 - отключен
+            2 - включен
+            3 - перемещается
+            '''
+            pos = (int(status) & 0xf0000) >> 16 #16-19 bits
+            '''
+            Положение радара:
+            0 - неопределённое
+            1 - поворачивается влево
+            2 - поворачивается вправо
+            3 - в крайнем левом положении
+            4 - в крайнем правом положении
+            '''
+            time_stamp = (int(status) & 0x1ffff00000) >> 20 #20-36 bits
+            '''
+            Время в мс с начала работы
+            '''
+            if dx > 2048:
+                dx = dx - 4096
+
+            previous_dx = (dx if dx != -1536 else previous_dx)
+            instant_velocity = (dx if pos != 3 and pos != 4 else 0)
+            instant_velocity = (instant_velocity if dx != -1536 else previous_velocity)
+            running_velocity = gamma * instant_velocity + (1 - gamma) * previous_velocity
+            current_rotation_speed = int(100 / max_speed * running_velocity)
+            previous_velocity = running_velocity
+
+            tracklog.write(f"Step number: {step_counter}\n".encode())
+            tracklog.write(f"Time stamp: {time_stamp}\n".encode())
+            tracklog.write(f"Bias: {dx}\n".encode())
+            tracklog.write(f"State: {state}\n".encode())
+            tracklog.write(f"Position: {pos}\n".encode())
+            tracklog.write(f"Instant velocity: {instant_velocity}\n".encode())
+            tracklog.write(f"Running velocity: {running_velocity}\n".encode())
+            tracklog.write(f"Current rotation speed: {current_rotation_speed}\n".encode())
+
+            #max velocity = 100, no_signal = -1536
+
+            if abs(running_velocity) < max_speed:
+                if abs(running_velocity) < int(max_speed/100):
+                    status = c2s.moveStop()
+                else:
+                    if running_velocity > 0:
+                        status = c2s.moveLeft(current_rotation_speed)
+                    else:
+                        status = c2s.moveRight(-1*current_rotation_speed)
+
+            step_counter += 1
--- unks/task1/test2/tracker.py
+++ unks/task1/test2/tracker.py
@@ -3,6 +3,8 @@ from client2server import client2server
 class tracker:
   def run(tracklog):
       c2s = client2server()
+      bias = 0
+      length = 0
       while True:
           status = c2s.getStatus()
           dx = int(status) & 0x0fff #0-12 bits
@@ -29,35 +31,55 @@ class tracker:
           '''
           if dx > 2048:
               dx = dx - 4096
+          if bias != dx:
+            tracklog.write("\n\nINFO\n----".encode())
+            tracklog.write(f"\nBias: {dx}".encode())
+            tracklog.write(f"\nState: {state}".encode())
+            tracklog.write(f"\nPosition: {pos}".encode())
+            tracklog.write(f"\nTime stamp: {time_stamp}".encode())
+            bias = dx
+            length = 0
+            # * в крайнем левом + неопределенное (состояние) -- вправо на 10
+            # * в крайнем правом + неопределенное (состояние) -- влево на 10
+            # ! поворачивается влево + dx < 0 -- вправо на 5
+            # ! поворачивается вправо + dx > 0 -- влево на 5
+            # ? (2) поворачивается вправо + неопределенное (состояние или положение) -- влево на 10
+            # ? (2) поворачивается влево + неопределенное (состояние или положение) -- вправо на 10
+            # time_stamp неопределенное (положение или состояние) типо преграда
+            if pos == 3:
+                #tracklog.write(f"\n\nBEFORE MOVING BIAS: {dx}\n----".encode())
+                c2s.moveRight(10)
+                #tracklog.write(f"\n\nAFTER START MOVING BIAS: {dx}\n----".encode())
+            elif pos == 4:
+                c2s.moveLeft(10)
+            elif pos == 1 and dx < 0:
+                c2s.moveRight(5)
+            elif pos == 2 and dx > 0:
+                c2s.moveLeft(5)  
+            elif abs(dx) < 1000:
+                if abs(dx) < 5:
+                    c2s.moveStop()
+                else:
+                    if dx > 0:
+                        c2s.moveLeft(5)
+                    else:
+                        c2s.moveRight(5)
 
-          tracklog.write(f"Bias: {dx}".encode())
-          tracklog.write(f"State: {state}".encode())
-          tracklog.write(f"Position: {pos}".encode())
-          tracklog.write(f"Time stamp: {time_stamp}".encode())
-          
-          # * в крайнем левом + неопределенное (состояние) -- вправо на 10
-          # * в крайнем правом + неопределенное (состояние) -- влево на 10
-          # ! поворачивается влево + dx < 0 -- вправо на 5
-          # ! поворачивается вправо + dx > 0 -- влево на 5
-          # ? (2) поворачивается вправо + неопределенное (состояние или положение) -- влево на 10
-          # ? (2) поворачивается влево + неопределенное (состояние или положение) -- вправо на 10
-          # time_stamp неопределенное (положение или состояние) типо преграда
-          if pos == 3:
-              c2s.moveRight(10)
-          if pos == 4:
-              c2s.moveLeft(10)
-          if pos == 0 or state == 0:
-              tracklog.write(f"Pos: {pos} " + f"\nState: {state} " + 
-                                f"\nTime stamp: {time_stamp}")
-          if pos == 1 and dx < 0:
-              c2s.moveRight(5)
-          if pos == 2 and dx > 0:
-              c2s.moveLeft(5)              
-          if abs(dx) < 1000:
-              if abs(dx) < 5:
-                  c2s.moveStop()
-              else:
-                  if dx > 0:
-                      c2s.moveLeft(5)
-                  else:
-                      c2s.moveRight(5)
+            if pos == 0 or state == 0:
+                tracklog.write("\n\nPOSITION OR STATE 0\n----".encode())
+                tracklog.write(f"\nPosition: {pos} ".encode())
+                tracklog.write(f"\nState: {state} ".encode())
+                tracklog.write(f"\nTime stamp: {time_stamp}".encode())
+                tracklog.write("\n----\n".encode()) 
+            if dx == -1536:
+                tracklog.write("\n|||||||||||||BARRIER?|||||||||||||\n".encode())   
+                tracklog.write(f"\nLENGTH: {length}".encode())
+                length += 1        
+        #   if abs(dx) < 1000:
+        #       if abs(dx) < 5:
+        #           c2s.moveStop()
+        #       else:
+        #           if dx > 0:
+        #               c2s.moveLeft(5)
+        #           else:
+        #               c2s.moveRight(5)
