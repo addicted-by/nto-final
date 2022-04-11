69da28a7b7a45c44a170bbee827d2ceab99821f4
Viktor Khaymonenko <khaimonenko.vb@phystech.edu>
2022-04-11 20:26:07 +0300

    Polygon is added

--- unks/client2server.py
+++ unks/client2server.py
@@ -1,15 +1,12 @@
 import numpy as np
+from bisect import bisect_right
 
 SPACE_LENGTH = 1000
-TOTAL_STEPS = 5000
-
-OBSTACLES_NUMBER_PROBA = 0.1
+TOTAL_STEPS = 1000
+MAX_OBSTACLE_LEN = 50
+OBSTACLES_NUMBER_PROBA = 0.4
 STEPS_NUMBER_PROBA = 0.4
 SPEED_STD = 10
-
-number_of_obstacles = np.random.geometric(p=OBSTACLES_NUMBER_PROBA)
-obstacles_positions = np.round(np.random.uniform(low=-SPACE_LENGTH, high=SPACE_LENGTH, size=number_of_obstacles))
-obstacles_positions.sort()
 
 
 class RadarState:
@@ -37,10 +34,10 @@ class Radar:
         self.current_position = 0
 
     def moveStop(self):
-        if self.current_position <= SPACE_LENGTH:
+        if self.current_position >= SPACE_LENGTH:
             self.current_position = SPACE_LENGTH
             self.position_state = RadarPositionState.EXTREME_RIGHT
-        elif self.current_position >= -SPACE_LENGTH:
+        elif self.current_position <= -SPACE_LENGTH:
             self.current_position = -SPACE_LENGTH
             self.position_state = RadarPositionState.EXTREME_LEFT
         else:
@@ -64,7 +61,7 @@ class Radar:
 
 
 class Satellite:
-    MAX_SPEED = 20
+    MAX_SPEED = 5
 
     def __init__(self):
         self.current_position = 0
@@ -79,17 +76,31 @@ class Satellite:
         self.current_position = max(-SPACE_LENGTH, self.current_position)
 
 
-class Tracker:
+class client2server:
     def __init__(self):
         self.radar = Radar()
         self.satellite = Satellite()
         self.timestamp = 0
+        self.log_file = open("c2s_log.txt", "w")
+
+        number_of_obstacles = np.random.geometric(p=OBSTACLES_NUMBER_PROBA)
+        left_obstacles = np.random.uniform(-SPACE_LENGTH, -MAX_OBSTACLE_LEN, size=number_of_obstacles // 2 * 2)
+        right_obstacles = np.random.uniform(MAX_OBSTACLE_LEN, SPACE_LENGTH, size=number_of_obstacles // 2 * 2)
+        left_obstacles.sort()
+        right_obstacles.sort()
+        self.obstacles_positions = np.concatenate([left_obstacles, right_obstacles])
 
     def getStatus(self):
-        self.timestamp += 1
-        dx = self.radar.current_position - self.satellite.current_position
+        dx = self.satellite.current_position - self.radar.current_position
         if abs(dx) > self.radar.VIEW:
             dx = -1516
+
+        between_obstacles_position = bisect_right(self.obstacles_positions, self.satellite.current_position)
+        if 0 < between_obstacles_position < len(self.obstacles_positions):
+            # even indices in self.obstacles_positions -- left corners of obstacles
+            # odd indices in self.obstacles_positions -- right corners of obstacles
+            if between_obstacles_position % 2 == 1:
+                dx = -1516
         state = self.radar.state
         position_state = self.radar.position_state
         timestamp = self.timestamp
@@ -104,4 +115,15 @@ class Tracker:
 
     def moveRight(self, speed: int):
         self.radar.moveRight(speed)
+
+    def iterate(self):
+        number_of_steps = 0
+        for self.timestamp in range(TOTAL_STEPS):
+            if number_of_steps == 0:
+                number_of_steps = np.random.geometric(p=STEPS_NUMBER_PROBA)
+                speed = np.round(np.random.normal(loc=0, scale=SPEED_STD))
+                self.log_file.write(f"Number of satellite steps {number_of_steps} with speed {speed}\n")
+            number_of_steps -= 1
+            self.satellite.step(speed)
+            yield self.getStatus()
 
